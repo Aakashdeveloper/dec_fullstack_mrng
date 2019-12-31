aed0649c488927dd91d8da94efaff32ccd5b81d4
aakashDeveloper <ahanda205@gmail.com>
2019-12-31 02:15:30 +0000

    a

---
+++ JavaScript/first.js
@@ -0,0 +1,134 @@
+String = "aaa", 'a' , '233', 'true'
+Number = 34, 46.464643, 43543645656
+Boolean = true/false
+
+//Es5(Ecma Script 5)
+var
+//Es6(Ecma Script 6)
+let 
+const 
+
+
+
+var a = 10
+var b = "hi"
+var c = true
+
+//////////////
+var a = 10
+undefined
+typeof(a)
+"number"
+var b = "hii"
+undefined
+typeof(b)
+"string"
+var c = 'javascript'
+undefined
+typeof(c)
+"string"
+var d = true;
+undefined
+typeof(d)
+"boolean"
+
+//////////
+var a = 10
+undefined
+var b = 5
+undefined
+a+b
+15
+a-b
+5
+a*b
+50
+a/b
+2
+
+var a = 10
+undefined
+var b = 2
+undefined
+a%b
+0
+var c =3
+undefined
+a%c
+1
+
+a
+10
+b
+2
+a%b
+0
+b%a
+2
+var c = 1
+undefined
+b%c
+0
+c%b
+1
+
+var a = "hi"
+undefined
+var b = " javascript"
+undefined
+a+b
+"hi javascript"
+a-b
+NaN
+a*b
+NaN
+a/b
+NaN
+a%b
+NaN
+var a = "hi"
+undefined
+var b = 10
+undefined
+a+b
+"hi10"
+b+a
+"10hi"
+
+string+string = string
+string+number = string
+number+string = string
+number+number = number
+
+1+1+"1"= "21"
+"1"+1+1= "111"
+1+"1"+1= "111"
+
+false == 0
+true
+true == 1
+true
+
+true + true
+2
+false + false 
+0
+
+true + true
+2
+false + false 
+0
+true+1
+2
+false-2
+-2
+false*2
+0
+true*2
+2
+true%false
+NaN
+false%true
+0
+1/0
+Infinity
---
+++ JavaScript/second.js
@@ -0,0 +1,57 @@
+= assignment operator(we are assigining value on left hand side)
+==  check data only
+===  check data and datatype
+
+
+var a = '10'
+var b = 10
+a == b
+true
+a === b
+false
+
+console.log('hii')
+VM1533:1 hii
+undefined
+alert("are you sure")
+undefined
+alert('form submitted')
+undefined
+confirm("are you done")
+true
+confirm("are you done")
+false
+var out = confirm("are you done")
+out = true
+
+prompt("Tell me your name")
+"john"
+var out = prompt("Tell me your name")
+undefined
+out
+null
+var out = prompt("Tell me your name")
+undefined
+out
+"john"
+var out = prompt("Tell me your name")
+undefined
+out
+"1"
+
+
+var a = prompt("first Number")
+var b = prompt("Second Number")
+a+b
+
+
+var a = prompt("first Number")
+var b = prompt("Second Number")
+a+b
+"12"
+var a = "10"
+undefined
+var b = "20"
+undefined
+a+b
+"1020"
