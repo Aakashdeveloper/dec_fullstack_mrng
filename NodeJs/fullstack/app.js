34ebde7700126c51a8cdc6d422042b2fbadde388
aakashDeveloper <ahanda205@gmail.com>
2020-01-17 02:27:13 +0000

    a

---
+++ NodeJs/fullstack/app.js
@@ -0,0 +1,111 @@
+var express = require('express');
+var app = express();
+var port = 9000;
+const moviesRouter = express.Router();
+const productRouter = express.Router();
+
+//Static files path
+app.use(express.static(__dirname+'/public'));
+//View Files
+app.set('views', './src/views');
+//View Engine
+app.set('view engine','ejs')
+
+
+var products = [
+
+    {
+      "_id": "5a05dacc734d1d68d42d31f3",
+      "productId": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.5,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    },
+    {
+      "_id": "5a05daec734d1d68d42d32ca",
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2016",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+    }
+  ]
+
+var movies = [
+    {
+        "_id": "5ab12612f36d2879268f284a",
+        "name": "Black Panther",
+        "language": "ENGLISH",
+        "rate": 4.5,
+        "type": "Action Adventure Fantasy",
+        "imageUrl": "https://image.ibb.co/f0hhZc/bp.jpg"
+      },
+      {
+        "_id": "5ab12666f36d2879268f2902",
+        "name": "Death Wish",
+        "language": "ENGLISH",
+        "type": "Action Crime Thriller",
+        "rate": 3.2,
+        "imageUrl": "https://image.ibb.co/gC9PfH/dw.jpg"
+      },
+      {
+        "_id": "5ab12678f36d2879268f291d",
+        "name": "Coco",
+        "language": "ENGLISH",
+        "type": "Adventure Animation Family",
+        "rate": 5,
+        "imageUrl": "https://image.ibb.co/dQwWSx/coco.jpg"
+      },
+      {
+        "_id": "5ab12678f36d2879268f291d",
+        "name": "Coco",
+        "language": "ENGLISH",
+        "type": "Adventure Animation Family",
+        "rate": 5,
+        "imageUrl": "https://image.ibb.co/dQwWSx/coco.jpg"
+      }
+]
+
+moviesRouter.route('/')
+    .get(function(req,res) {
+        //res.send(movies)
+        res.render('movies',{title:'Movies Page', movies:movies})
+    });
+
+moviesRouter.route('/details')
+    .get(function(req,res) {
+        res.send("Details for Movies")
+    });
+
+
+productRouter.route('/')
+    .get(function(req,res){
+        //res.send(products)
+        res.render('products',{title:'Products Page',products:products})
+    })
+
+productRouter.route('/details')
+    .get(function(req,res) {
+        res.send("Details for Products")
+    });
+
+app.get('/', function(req,res){
+    res.send("Details for Products")
+});
+
+app.use('/movies', moviesRouter);
+app.use('/products',productRouter)
+
+app.listen(port, (err)=>{
+    if(err) throw err;
+    else{
+        console.log(`Server is running on port ${port}`)
+    }
+});
